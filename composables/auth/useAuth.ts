ca0f92a3e143fdace66c08ee10b63526bad604ac
PouryaNoufallah96 <pouryanoufallah@yahoo.com>
2022-09-06 20:17:21 +0430

    test

--- composables/auth/useAuth.ts
+++ composables/auth/useAuth.ts
@@ -49,7 +49,7 @@ export const useAuth = () => {
             }
             catch (err) {
                 setUser(null);
-                setToken(null);
+                // setToken(null);
             }
         }
     }
